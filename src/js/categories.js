ddd7a6a101cca12a8431cb5be236003a8a69dcc8
OlhaNazarova <108295915+OlhaNazarova@users.noreply.github.com>
2024-03-02 13:00:39 +0200

    render all categories list in html

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,63 +1,100 @@
-// function makeCategoryBooksMarkup(data) {
-//     return `
-//         <ul class="category-books-list">
-//           ${data
-//             .map(element => {
-//               return `
-//               <li class="books-list-item">
-//                 ${createBookMarkup(element)}
-//               </li>
-//             `;
-//             })
-//             .join('')}
-//         </ul>
-//       `;
-//   }
-// -----------------------------------------
-// function createBookMarkup({
-//     _id: id,
-//     title,
-//     author,
-//     description,
-//     book_image: image,
-//   }) {
-//     return `
-//       <a class="book-link" href="" aria-label="Book thumbnail">
-//         <div class="book-thumb">
-//           <img class="book-image" src="${image}" loading="lazy" data_id=${id} alt="${description}"/>
-//           <div class="book-image-overlay" data_id=${id} aria-label="${title}">
-//             <p class="book-image-overlay-text">Quick view</p>
-//           </div>
-//         </div>
-//         <div class="book-item-meta">
-//           <h3 class="book-title">${title}</h3>
-//           <p class="book-author">${author}</p>
-//         </div>
-//       </a>
-//     `;
+import axios from 'axios';
+
+
+const categoryListEl = document.querySelector('.categories-list');
+
+console.log(categoryListEl);
+
+const categoriesArray = async () => {
+  const response = await axios.get(
+    'https://books-backend.p.goit.global/books/category-list'
+  );
+  const data = await response;
+
+  return data;
+};
+
+
+
+export async function renderCategories() {
+  try {
+    const array = await categoriesArray();
+    const inAlphabeticalOrder = array.data.sort((a, b) =>
+      a.list_name.localeCompare(b.list_name)
+    );
+console.log(inAlphabeticalOrder);
+    for (const arr of inAlphabeticalOrder) {
+      const listItem = document.createElement('li');
+      const catLink = document.createElement('a');
+
+      catLink.classList.add('categories-link');
+      catLink.textContent = `${arr.list_name}`;
+      listItem.setAttribute('data-id', arr.list_name);
+      categoryListEl.appendChild(listItem);
+      listItem.appendChild(catLink);
+    }
+    console.log(array.data);
+
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+// -----------------------1category
+
+// import Notiflix from 'notiflix';
+// categoryListEl.addEventListener('click', onCategoryClick);
+
+// async function onCategoryClick(event) {
+//   event.preventDefault();
+
+//   if (!event.target.classList.contains('category-btn')) return;
+
+//   if (event.target.classList.contains('all-categories-btn')) {
+//     mainTitle.innerHTML = `Best Seller <span class="main-title--last-word-static">Books</span>`;
+  
+//     await renderTopBooks();
+//     return;
 //   }
 
-//   -----------------------------------------
-// export function renderBooksByCategory(data) {
-//     const markup = makeCategoryBooksMarkup(data);
-//     renderingContainer.innerHTML = markup;
-//   }
+//   const id = event.target.dataset.id;
 
-//   -------------------------------------------
-// export async function renderCategories() {
+//   const data = await fetchSelectedBooks(id);
+//   renderMainTitle(id);
 
-//     const arr = [{list_name: "test1"}, {list_name: "test2"}]
-//         for (const arr of inAlphabeticalOrder) {
-//           const listItem = document.createElement('li');
-//           const catBtn = document.createElement('button');
-//           catBtn.setAttribute('type', 'button');
-//           catBtn.classList.add('category-btn');
-//           catBtn.textContent = ${arr.list_name};
-//           catBtn.setAttribute('data-id', arr.list_name);
-//           categoryListEl.appendChild(listItem);
-//           listItem.appendChild(catBtn);
-//         }
-//     }
+//   renderBooksByCategory(data);
+//   scrollToTop();
+// }
+
+// export async function fetchSelectedBooks(category) {
+// //   Notiflix.Loading.dots('Please wait');
+//   const categoriesArray = async category => {
+//     const response = await axios.get(
+//       `https://books-backend.p.goit.global/books/category?category=${category}`
+//     );
+//     const data = await response;
     
-//     renderCategories();
-//     ----------------------------------------
+//     // Notiflix.Loading.remove();
+//     return data;
+//   };
+//   try {
+   
+//     // Notiflix.Loading.remove();
+  
+//     return categoriesArray(category);
+//   } catch (error) {
+//     // Notiflix.Loading.remove();
+//     // Notiflix.Notify.failure('Something went wrong. Please try again');
+//     console.log(error);
+//   }
+// }
+// categoryListEl.addEventListener('click', onCategoryClick);
+//  async function onCategoryClick(event) {
+//     const link = event.target.classList.contains('categories-link');
+//     console.log(link);
+//     const id = event.target.dataset;
+//     console.log(id);
+// }
+// onCategoryClick();
+
+renderCategories();
