25e3d8f1914bb749330e14c83a089974ea75daf7
Artem <atremat@gmail.com>
2024-03-02 20:01:26 +0200

    primitive localstorage added

--- src/js/modal-authorization.js
+++ src/js/modal-authorization.js
@@ -1,4 +1,5 @@
 import * as basicLightbox from 'basiclightbox';
+import { addToLS } from './local-storage-functions';
 import { initializeApp } from 'firebase/app';
 import {
   getAuth,
@@ -6,75 +7,7 @@ import {
   signInWithEmailAndPassword,
 } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyAU9vmqTZLyoAQQ-FXLYuEWvBcdBAw2N_s',
-  authDomain: 'bookworms-de9f1.firebaseapp.com',
-  projectId: 'bookworms-de9f1',
-  storageBucket: 'bookworms-de9f1.appspot.com',
-  messagingSenderId: '67746495730',
-  appId: '1:67746495730:web:2051c7fd00eaf14945646b',
-};
-
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth();
-
-// sign up of new user
-const registerWithEmailAndPassword = async (email, password) => {
-  try {
-    const userCredential = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    const user = userCredential.user;
-    console.log('Registered user:', user);
-    return user;
-  } catch (error) {
-    console.error('Registration error:', error.message);
-    throw error;
-  }
-};
-
-// sign in of existing user
-// const signInWithEmailAndPassword = async (email, password) => {
-//   try {
-//     const userCredential = await signInWithEmailAndPassword(
-//       auth,
-//       email,
-//       password
-//     );
-//     const user = userCredential.user;
-//     console.log('Signed in user:', user);
-//     return user;
-//   } catch (error) {
-//     console.error('Sign-in error:', error.message);
-//     throw error;
-//   }
-// };
-
-// sign up
-const handleRegistration = async () => {
-  try {
-    await registerWithEmailAndPassword(email, password);
-    //  additional actions after successfull sign up
-  } catch (error) {
-    // handle errors
-  }
-};
-
-// sign in
-const handleSignIn = async () => {
-  try {
-    await signInWithEmailAndPassword(email, password);
-    //  additional actions after successfull sign in
-  } catch (error) {
-    // handle errors
-  }
-};
-
-//! ///////////////////////////
+const AUTH_KEY_LS = 'auth-info';
 
 //open authoriztion modal
 export function openAuthModal() {
@@ -159,6 +92,7 @@ export function openAuthModal() {
   const authCloseBtn = document.querySelector('.auth-close-btn');
   const authModal = document.querySelector('.auth-modal');
   const authNameInput = document.querySelector('#auth-name');
+  const authForm = document.querySelector('.auth-form');
   const authSubmitBtn = document.querySelector('.auth-submit-btn');
   const authSignInBtn = document.querySelector('.sign-in-btn');
   const authSignUpBtn = document.querySelector('.sign-up-btn');
@@ -190,6 +124,40 @@ export function openAuthModal() {
     //renaming button
     authSubmitBtn.textContent = 'Sign up';
   });
+
+  authForm.addEventListener('submit', onAuthFormSubmit);
+
+  function onAuthFormSubmit(e) {
+    e.preventDefault();
+
+    //value of button ==> sign in or sign up check
+    const authBtnValue = e.target.elements[3].textContent.toLowerCase();
+
+    //sign in or sign up check
+    if (authBtnValue === 'sign up') {
+      const userName = e.target.elements.name.value.trim();
+      const userEmail = e.target.elements.email.value.trim();
+      const userPassword = e.target.elements.password.value.trim();
+      const userInfo = {
+        userName,
+        userEmail,
+        userPassword,
+      };
+      console.log(userInfo);
+      addToLS(AUTH_KEY_LS, userInfo);
+    }
+
+    if (authBtnValue === 'sign in') {
+      //has to be check if user/pass is correct
+      //and change state of vars
+      //global let isSignedIn = true;
+      //global let authUser = userName
+      //and close modal
+      //authInstance.close();
+      //header button shows authUser value
+      console.log('log in success');
+    }
+  }
 }
 
 // for header.js
@@ -199,4 +167,78 @@ const authOpenModalBtn = document.querySelector(
   'button[data-action="registration"]'
 );
 
+//check if button 'sign up' in header is pressed
 authOpenModalBtn.addEventListener('click', openAuthModal);
+
+//! //////////////////////////////////////////////////////////
+//! firebase
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAU9vmqTZLyoAQQ-FXLYuEWvBcdBAw2N_s',
+  authDomain: 'bookworms-de9f1.firebaseapp.com',
+  projectId: 'bookworms-de9f1',
+  storageBucket: 'bookworms-de9f1.appspot.com',
+  messagingSenderId: '67746495730',
+  appId: '1:67746495730:web:2051c7fd00eaf14945646b',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+// sign up of new user
+const registerWithEmailAndPassword = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    console.log('Registered user:', user);
+    return user;
+  } catch (error) {
+    console.error('Registration error:', error.message);
+    throw error;
+  }
+};
+
+// sign in of existing user
+// const signInWithEmailAndPassword = async (email, password) => {
+//   try {
+//     const userCredential = await signInWithEmailAndPassword(
+//       auth,
+//       email,
+//       password
+//     );
+//     const user = userCredential.user;
+//     console.log('Signed in user:', user);
+//     return user;
+//   } catch (error) {
+//     console.error('Sign-in error:', error.message);
+//     throw error;
+//   }
+// };
+
+// sign up
+const handleRegistration = async () => {
+  try {
+    await registerWithEmailAndPassword(email, password);
+    //  additional actions after successfull sign up
+  } catch (error) {
+    // handle errors
+  }
+};
+
+// sign in
+const handleSignIn = async () => {
+  try {
+    await signInWithEmailAndPassword(email, password);
+    //  additional actions after successfull sign in
+  } catch (error) {
+    // handle errors
+  }
+};
+
+//! ///////////////////////////
