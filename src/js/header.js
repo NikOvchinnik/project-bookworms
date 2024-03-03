768023c7f9bff523bb4135f02f31003e455689ba
NikOvchinnik <136114400+NikOvchinnik@users.noreply.github.com>
2024-03-03 05:03:11 +0000

    Deploying to gh-pages from @ NikOvchinnik/project-bookworms@de6444b51cb0b9ce1525fe1b70f627e662bf500b 🚀

--- src/js/header.js
+++
@@ -1,8 +0,0 @@
-const toggleSwitch = document.getElementById('toggle');
-        const switchSlider = document.querySelector('.switch-slider');
-        const labelHeader = document.querySelector('.label-header');
-
-        toggleSwitch.addEventListener('change', function () {
-            switchSlider.classList.toggle('active');
-            labelHeader.style.background = this.checked ? 'linear-gradient(180deg, #4f2ee8 0%, #686868 100%)' : 'linear-gradient(180deg, #4f2ee8 0%, #fff 100%)';
-        });
